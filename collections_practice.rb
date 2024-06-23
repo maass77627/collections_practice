698e3500e5813e1b9f5fb0b00231d9f049e6bdac
kelly_maass <kmaass77627@gmail.com>
2024-06-23 18:59:16 -0500

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 
+def sort_array_asc(array)
+    array.sort
+
+end
+
+def sort_array_desc(array)
+    array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+    array.sort {|a,b| a.length <=> b.length}
+
+end
+
+def swap_elements(array)
+   
+array[1], array[2] = array[2], array[1]
+array
+
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    newarray = []
+    array.each do |word|
+            word[2] = "$"
+        newarray << word
+end
+newarray
+end
+
+def find_a(array)
+    array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    newarray = []
+   array.each do |word|
+    word << "s"
+    newarray << word
+   end
+   newarray[1] = "feet"
+   newarray
+end
+
+
